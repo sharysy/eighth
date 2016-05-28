901a714bef3fb716f1c2f6005da0e54401ff27bb
sharysy <1287641592@qq.com>
2016-05-28 14:19:19 +0800

    8

---
+++ src/collect_all_even.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function collect_all_even(collection) {
+    //在这里写入代码
+    var i, arr = [];
+    for (i = 0; i < collection.length; i++)
+        if (collection[i] % 2 === 0)
+            arr.push(collection[i]);
+    return arr;
+}
+
+module.exports = collect_all_even;
